41e4edfc8db9df3b163a679d284ac55c9fc4cb4f
Morgan Bastian <morganbastian07@gmail.com>
2023-07-12 12:56:55 -0400

    centered footer

--- ui/src/layouts/index.js
+++ ui/src/layouts/index.js
@@ -1,15 +1,28 @@
-import { Paper } from '@mui/material'
-import { Outlet } from 'react-router-dom'
-import Navbar from './Navbar'
-import Footer from './Footer'
+import { Paper, Grid } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = (props) => {
-  return (
-    <Paper>
-      <Navbar/>
-      <Outlet/> {/* Outlet is not a “real” Component, it is imported from react-router-dom. it will be replaced by the element rendered by the route */}
-      <Footer/>
-    </Paper>
-  )
-}
-export default Layout
+	return (
+		<Paper>
+			<Navbar />
+			<Outlet />{' '}
+			{/* Outlet is not a “real” Component, it is imported from react-router-dom. it will be replaced by the element rendered by the route */}
+			<Grid
+				container
+				spacing={0}
+				direction='column'
+				alignItems='center'
+				justifyContent='center'
+				sx={{ minHeight: '100vh' }}
+			>
+				<Grid item xs={3}>
+					<Footer />
+				</Grid>
+			</Grid>
+		</Paper>
+	);
+};
+
+export default Layout;
