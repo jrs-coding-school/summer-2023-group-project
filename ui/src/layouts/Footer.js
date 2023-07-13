b68775617355761ae4cd795686417120341489ca
Morgan Bastian <morganbastian07@gmail.com>
2023-07-13 10:20:01 -0400

    reworked footer

--- ui/src/layouts/Footer.js
+++ ui/src/layouts/Footer.js
@@ -1,47 +1,130 @@
-import { Paper } from '@mui/material';
-import Typography from '@mui/material/Typography';
+// import { Box, Paper } from '@mui/material';
+// import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
+
+// function Footer(props) {
+// 	return (
+// 		<Box
+// 			sx={{
+// 				display: 'flex',
+// 				flexDirection: 'column',
+// 				minHeight: '100vh',
+// 				borderTop: '1px solid #000',
+// 				marginTop: 'auto',
+// 				p: 4,
+// 			}}
+// 		>
+// 			<Typography variant='h6'>Created By:</Typography>
+// 			<Typography>
+// 				<Link to='https://www.linkedin.com/in/jackson-abeyta-960243281/'>
+// 					Jackson Abeyta
+// 				</Link>
+// 			</Typography>
+
+// 			<Typography>
+// 				<Link to='https://www.linkedin.com/in/morgan-bastian-a6a715181/'>
+// 					Morgan Bastian
+// 				</Link>
+// 			</Typography>
+
+// 			<Typography>
+// 				<Link to='https://www.linkedin.com/in/tina-funderburk-5440b2255/'>
+// 					Tina Funderburk
+// 				</Link>
+// 			</Typography>
+
+// 			<Typography>
+// 				<Link to='https://www.linkedin.com/in/mckain-badger-131044237/'>
+// 					McKain Badger
+// 				</Link>
+// 			</Typography>
+
+// 			<Typography>
+// 				<Link to='https://www.linkedin.com/in/colt-andrews-717bb11a6/'>
+// 					Colt Andrews
+// 				</Link>
+// 			</Typography>
+
+// 			<Typography>
+// 				<Link to='https://www.linkedin.com/in/william-godfrey-492568278/'>
+// 					William Godfrey
+// 				</Link>
+// 			</Typography>
+// 		</Box>
+// 	);
+// }
+
+import React from 'react';
+import { Box, Container, Grid, Typography } from '@mui/material';
 
 function Footer(props) {
 	return (
-		<Paper>
-			<Typography variant='h6'>Created By:</Typography>
-			<Typography>
-				<Link to='https://www.linkedin.com/in/jackson-abeyta-960243281/'>
-					Jackson Abeyta
-				</Link>
-			</Typography>
-
-			<Typography>
-				<Link to='https://www.linkedin.com/in/morgan-bastian-a6a715181/'>
-					Morgan Bastian
-				</Link>
-			</Typography>
-
-			<Typography>
-				<Link to='https://www.linkedin.com/in/tina-funderburk-5440b2255/'>
-					Tina Funderburk
-				</Link>
-			</Typography>
-
-			<Typography>
-				<Link to='https://www.linkedin.com/in/mckain-badger-131044237/'>
-					McKain Badger
-				</Link>
-			</Typography>
-
-			<Typography>
-				<Link to='https://www.linkedin.com/in/colt-andrews-717bb11a6/'>
-					Colt Andrews
-				</Link>
-			</Typography>
-
-			<Typography>
-				<Link to='https://www.linkedin.com/in/william-godfrey-492568278/'>
-					William Godfrey
-				</Link>
-			</Typography>
-		</Paper>
+		<Box
+			sx={{
+				position: 'fixed',
+				width: '100%',
+				height: 'auto',
+				// backgroundColor: 'secondary.main',
+				paddingTop: '1rem',
+				paddingBottom: '1rem',
+			}}
+		>
+			<Container maxWidth='lg'>
+				<Grid container direction='column' alignItems='center'>
+					<Grid item xs={12}>
+						<Typography color='black' variant='h5'>
+							Created By
+						</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Typography color='textSecondary' variant='subtitle1'>
+							<Link
+								to='https://www.linkedin.com/in/jackson-abeyta-960243281/'
+								style={{ textDecoration: 'none' }}
+							>
+								Jackson Abeyta
+							</Link>
+							<span> | </span>
+							<Link
+								to='https://www.linkedin.com/in/morgan-bastian-a6a715181/'
+								style={{ textDecoration: 'none' }}
+							>
+								Morgan Bastian
+							</Link>
+							<span> | </span>
+							<Link
+								to='https://www.linkedin.com/in/tina-funderburk-5440b2255/'
+								style={{ textDecoration: 'none' }}
+							>
+								Tina Funderburk
+							</Link>
+							<span> | </span>
+							<Link
+								to='https://www.linkedin.com/in/mckain-badger-131044237/'
+								style={{ textDecoration: 'none' }}
+							>
+								McKain Badger
+							</Link>
+							<span> | </span>
+							<Link
+								to='https://www.linkedin.com/in/colt-andrews-717bb11a6/'
+								style={{ textDecoration: 'none' }}
+							>
+								Colt Andrews
+							</Link>
+							<span> | </span>
+							<Link
+								to='https://www.linkedin.com/in/william-godfrey-492568278/'
+								style={{ textDecoration: 'none' }}
+							>
+								William Godfrey
+							</Link>
+							<span> | </span>
+						</Typography>
+					</Grid>
+				</Grid>
+			</Container>
+		</Box>
 	);
 }
 
--- ui/src/layouts/index.js
+++ ui/src/layouts/index.js
@@ -1,4 +1,4 @@
-import { Paper, Grid } from '@mui/material';
+import { Paper } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -7,20 +7,9 @@ const Layout = (props) => {
 	return (
 		<Paper>
 			<Navbar />
-			<Outlet />{' '}
+			<Outlet />
 			{/* Outlet is not a “real” Component, it is imported from react-router-dom. it will be replaced by the element rendered by the route */}
-			<Grid
-				container
-				spacing={0}
-				direction='column'
-				alignItems='center'
-				justifyContent='center'
-				sx={{ minHeight: '100vh' }}
-			>
-				<Grid item xs={3}>
-					<Footer />
-				</Grid>
-			</Grid>
+			<Footer />
 		</Paper>
 	);
 };
