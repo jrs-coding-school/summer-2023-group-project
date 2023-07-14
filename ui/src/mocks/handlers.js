07f82fdce716bee447aa87cbcf91b683edb285e9
William Godfrey <willyhg20@gmail.com>
2023-07-14 14:32:11 -0400

    copied mock data and set up MSW

--- ui/src/mocks/handlers.js
+++ ui/src/mocks/handlers.js
@@ -1,9 +1,10 @@
 import { rest } from 'msw'
+import mockUserData from '../mocks/data/mockUserData.json'
 
 export const handlers = [
-  // rest.get('/greeting', (req, res, ctx) => { // capture "GET /greeting" requests
-  //   return res(ctx.json({greeting: 'hello there'})) // respond using a mocked JSON body
-  // }),
+  rest.get('http://localhost:9000/users/', (req, res, ctx) => { // capture "GET /greeting" requests
+    return res(ctx.json(mockUserData)) // respond using a mocked JSON body
+  }),
   // rest.get('/farewell', (req, res, ctx) => { // capture "GET /greeting" requests
   //   return res(ctx.json({farewell: 'goodbye there'})) // respond using a mocked JSON body
   // }),
--- ui/src/views/Home/index.js
+++ ui/src/views/Home/index.js
@@ -1,24 +1,39 @@
-import { Paper } from '@mui/material'
-import { Map, Marker } from "pigeon-maps"
-import {getToken, isUserLoggedIn, setToken, clearToken} from '../../utility/utils'
-import { Link} from 'react-router-dom'
-import Button from '@mui/material/Button';
-function Home (props) {
+import { Paper } from "@mui/material";
+import { Map, Marker } from "pigeon-maps";
+import {
+  getToken,
+  isUserLoggedIn,
+  setToken,
+  clearToken,
+} from "../../utility/utils";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+
+function Home(props) {
   return (
     <Paper>
-      {isUserLoggedIn() ? <Map
-       height={600} defaultCenter={[32.7765, -79.9311]} defaultZoom={13}>
-      <Marker width={50} anchor={[32.7765, -79.9311]} />
-    </Map>:<Link to='/login'><Button sx={{
-										// display: { xs: 'none', md: 'flex' },
-										mr: 1,
-										textDecoration: 'none',
-										color: 'white',
-										flex: 'row-reverse',
-									}}>Must Login to View Map</Button></Link> }
-      
+      {isUserLoggedIn() ? (
+        <Map height={600} defaultCenter={[32.7765, -79.9311]} defaultZoom={13}>
+          <Marker width={50} anchor={[32.7765, -79.9311]} />
+        </Map>
+      ) : (
+        <Link to="/login">
+          <Button
+            sx={{
+              // display: { xs: 'none', md: 'flex' },
+              mr: 1,
+              textDecoration: "none",
+              color: "white",
+              flex: "row-reverse",
+            }}
+          >
+            Must Login to View Map
+          </Button>
+        </Link>
+      )}
     </Paper>
-  )
+  );
 }
 
-export default Home
+export default Home;
