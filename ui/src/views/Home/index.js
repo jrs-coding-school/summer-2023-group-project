e5f79457a81ed3be8071d63305acdb491df9d379
William Godfrey <willyhg20@gmail.com>
2023-07-14 11:58:23 -0400

    added map and default marked Charleston Co-authored-by: Tina Funderburk

--- ui/src/views/Home/index.js
+++ ui/src/views/Home/index.js
@@ -1,9 +1,13 @@
 import { Paper } from '@mui/material'
-
+import { Map, Marker } from "pigeon-maps"
 function Home (props) {
   return (
     <Paper>
       Home
+      <Map
+       height={300} defaultCenter={[32.7765, -79.9311]} defaultZoom={11}>
+      <Marker width={50} anchor={[32.7765, -79.9311]} />
+    </Map>
     </Paper>
   )
 }
