6ad90060563b7d404f47031e019234de54d27069
William Godfrey <willyhg20@gmail.com>
2023-07-14 15:00:19 -0400

    MSW working state

--- ui/src/views/Home/index.js
+++ ui/src/views/Home/index.js
@@ -8,9 +8,38 @@ import {
 } from "../../utility/utils";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
+import { useParams } from 'react-router-dom'
+import { useEffect , useState } from 'react'
 
 
 function Home(props) {
+  const formatting = '. '
+  const [data, setData] = useState(null)
+  const { userId } = useParams()
+  useEffect(() => {
+    fetch(`http://localhost:9000/users/id/1`)
+      .then((response) => {
+        if(!response.ok) {
+          throw new Error('failed to fetch data')
+        }
+        return response.json() // parse the response data
+      })
+      .then((result) => {
+        setData(result)
+    }) // set state when the data received
+      .catch((err) => err) // return the error
+    }, [])
+    if (!data) { // guard clause to prevent runtime errors
+      return ( 
+        <div>
+          <div>Loading...</div>
+        </div>
+        )
+    }
+    console.log(data)
+
+
+
   return (
     <Paper>
       {isUserLoggedIn() ? (
