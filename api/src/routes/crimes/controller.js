e6f6a99fe1355443835859894e901a04fce71137
Amin Yachnes <amin@harborec.com>
2023-07-21 12:56:35 -0400

    fix import errors

--- api/src/routes/crimes/controller.js
+++ api/src/routes/crimes/controller.js
@@ -1,31 +1,33 @@
-require('dotenv').config()
-const { findAllCrimes, findReportById, findCrimeById } = require('./service')
+require("dotenv").config()
+const { findAllCrimes, findReportById, findCrimeById } = require("./service")
 
 exports.getAllCrimes = async (req, res) => {
   try {
-
     // Only allow admins and account owners to access the user data
     const foundCrimes = await findAllCrimes()
 
     if (!foundCrimes) {
-      return res.status(404).json('No User Found')
+      return res.status(404).json("No User Found")
     }
-    
-    return res.json(foundCrimes)
 
+    return res.json(foundCrimes)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json()
+  }
+}
 exports.showCrimeById = async (req, res) => {
   try {
     // Only allow admins and account owners to access the user data
     const foundCrime = await findCrimeById(req.params.id)
 
     if (!foundCrime) {
-      return res.status(404).json('No User Found')
+      return res.status(404).json("No User Found")
     }
-    
+
     return res.json(foundCrime)
   } catch (error) {
     console.log(error)
     return res.status(500).json()
   }
-
 }
--- api/src/routes/reports/controller.js
+++ api/src/routes/reports/controller.js
@@ -1,39 +1,48 @@
-require('dotenv').config()
+require("dotenv").config()
 
-const { findAllReports, findReportsByCounty, findReportById, addNewReport, findReportByUserId, modifyUserReports, destroyUserReport} = require('./service')
+const {
+  findAllReports,
+  findReportsByCounty,
+  findReportById,
+  addNewReport,
+  findReportByUserId,
+  modifyUserReports,
+  destroyUserReport,
+} = require("./service")
 
 exports.showReportById = async (req, res) => {
   try {
-
     // Only allow admins and account owners to access the user data
     const foundReport = await findReportById(req.params.id)
 
     if (!foundReport) {
-      return res.status(404).json('No User Found')
+      return res.status(404).json("No User Found")
     }
-    
+
     return res.json(foundReport)
   } catch (error) {
     console.log(error)
     return res.status(500).json()
   }
-
 }
 
 exports.showReportByUserId = async (req, res) => {
   try {
-
     // Only allow admins and account owners to access the user data
     const foundReport = await findReportByUserId(req.params.userId)
 
     if (!foundReport) {
-      return res.status(404).json('No User Found')
+      return res.status(404).json("No User Found")
     }
-    
+
     return res.json(foundReport)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json()
+  }
+}
 exports.createNewReport = async (req, res) => {
   try {
-
     const newReport = req.body
     // Only allow admins and account owners to access the user data
     const report = await addNewReport(newReport)
@@ -42,70 +51,70 @@ exports.createNewReport = async (req, res) => {
     console.log(error)
     return res.status(500).json()
   }
+}
 
 exports.showAllReports = async (req, res) => {
-    try {
-      const allReports = await findAllReports(req.params)
-      console.log('allReports: ', allReports)
-      return res.json(allReports)
-  
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json()
-    }
+  try {
+    const allReports = await findAllReports(req.params)
+    console.log("allReports: ", allReports)
+    return res.json(allReports)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json()
   }
-  
-  exports.showCountyReports = async (req, res) => {
-    console.log(req.params.county)
-    try {
-      const countyReports = await findReportsByCounty(req.params.county)
-      console.log('countyReports: ', countyReports)
-      return res.json(countyReports)
-  
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json()
-    }
+}
+
+exports.showCountyReports = async (req, res) => {
+  console.log(req.params.county)
+  try {
+    const countyReports = await findReportsByCounty(req.params.county)
+    console.log("countyReports: ", countyReports)
+    return res.json(countyReports)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json()
   }
+}
 
-  exports.updateUserReports = async (req, res) => {
-    const reportId = req.params.id
-    const newReportData = req.body
-    console.log("user", req.user)
-    try {
+exports.updateUserReports = async (req, res) => {
+  const reportId = req.params.id
+  const newReportData = req.body
+  console.log("user", req.user)
+  try {
+    const reportData = await findReportById(reportId)
 
-      const reportData = await findReportById(reportId)
-
-      // Only allow users to edit their own reports unless the user is an admin
-      if (req.user.id !== reportData.userId && req.user.role !== 'admin') {
-        return res.status(403).json({ error: 'You do not have permission to access this resource' })
-      }
-  
-      const updatedUserReport =  await modifyUserReports(newReportData, reportId)
-      return res.json(updatedUserReport)
-  
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json()
+    // Only allow users to edit their own reports unless the user is an admin
+    if (req.user.id !== reportData.userId && req.user.role !== "admin") {
+      return res
+        .status(403)
+        .json({ error: "You do not have permission to access this resource" })
     }
-  }
 
-  exports.deleteUserReport = async (req, res) => {
-    const reportId = req.params.id
-    try {
-      console.log("reportId:", reportId)
-      const reportData = await findReportById(reportId)
-      console.log("reportData:", reportData)
-      // Only allow admins to access the user list
-      if (req.user.id !== reportData.userId && req.user.role !== 'admin') {
-        return res.status(403).json({ error: 'You do not have permission to access this resource' })
-      }
-  
-      const deletedUserReport = await destroyUserReport(reportId)
-      return res.json(deletedUserReport)
-  
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json()
-    }
+    const updatedUserReport = await modifyUserReports(newReportData, reportId)
+    return res.json(updatedUserReport)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json()
   }
+}
+
+exports.deleteUserReport = async (req, res) => {
+  const reportId = req.params.id
+  try {
+    console.log("reportId:", reportId)
+    const reportData = await findReportById(reportId)
+    console.log("reportData:", reportData)
+    // Only allow admins to access the user list
+    if (req.user.id !== reportData.userId && req.user.role !== "admin") {
+      return res
+        .status(403)
+        .json({ error: "You do not have permission to access this resource" })
+    }
+
+    const deletedUserReport = await destroyUserReport(reportId)
+    return res.json(deletedUserReport)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json()
+  }
+}
