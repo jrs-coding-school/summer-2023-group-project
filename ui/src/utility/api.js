b63d3196dc9893de4233c75466303ed235380f03
Amin Yachnes <amin@harborec.com>
2023-07-21 12:56:44 -0400

    fix import errors

--- ui/src/utility/api.js
+++ ui/src/utility/api.js
@@ -86,9 +86,6 @@ export const updateUserById = async(data) => {
   return responseData
 }
 
-export const getAllUsers = async(data) => {
-
-
 export const getAllUsers = async (data) => {
   const token = getToken();
   if (!token) {
@@ -350,20 +347,6 @@ export const getLocationByAddress = async (search) => {
 
   if (!response.ok) {
     throw new Error(`failed to get coords`)
-  }
-
-  return responseData
-}
-  
-export const getReportById = async(id) => {
-
-  const response = await fetch(`${baseUrl}/reports/${id}`, {
-    method: "GET",
-  })
-
-  const responseData = await response.json()
-  if (!response.ok) {
-    throw new Error(`Status Code: ${response?.status} - ${responseData?.message}`)
   }
 
   return responseData
